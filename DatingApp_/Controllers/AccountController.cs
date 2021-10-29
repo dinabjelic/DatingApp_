61616115a3bb96a18ed6cbe5b9681a084afa0bbf
dinabjelic <75682425+dinabjelic@users.noreply.github.com>
2021-10-29 19:15:35 +0200

    Add project files.

---
+++ DatingApp_/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using DatingApp.API.DTOs;
+using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost("register")]
+
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO register)
+        {
+            if(await UserExists(register.UserName))
+            {
+                return BadRequest("UserName is taken");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = register.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
+                PasswordSalt = hmac.Key
+
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user; 
+
+             
+
+        }
+        private async Task<bool> UserExists(string UserName)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == UserName.ToLower());
+
+        }
+
+      
+
+    }
+}
